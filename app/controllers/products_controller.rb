bd11a408d3d816a26f37c04a33d0d69921c25bc8
SettingTom <tnbt0277@gmail.com>
2022-04-30 11:24:12 +0900

    ストロングパラメータの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :text, :image).merge(user_id: current_user.id)
+  end
 end
