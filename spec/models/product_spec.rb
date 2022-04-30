4012daafb5647cce63e4a7e9089bd3254b40ce45
SettingTom <tnbt0277@gmail.com>
2022-04-30 19:54:28 +0900

    productモデルのテストコードの実装

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '作品投稿' do
+    context '作品投稿できるとき' do
+      it 'name、text、imageが存在すれば登録できる' do
+        expect(@product).to be_valid
+      end
+    end
+
+    context '作品投稿できないとき' do
+      it 'nameが空では登録できない' do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'textが空では登録できない' do
+        @product.text = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'imageが空では登録できない' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていなければ登録できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
