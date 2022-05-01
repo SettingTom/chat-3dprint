88fe6cfcb1c4b00636bf588c1018cf81a2b03739
SettingTom <tnbt0277@gmail.com>
2022-05-01 18:21:03 +0900

    編集内容保存機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,12 @@ class ProductsController < ApplicationController
   end
 
   def update
-    
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
   end
 
   private
