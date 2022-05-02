f218e8aae52f68d9159d564b0b5cf042bcad7174
SettingTom <tnbt0277@gmail.com>
2022-05-02 12:37:02 +0900

    削除機能の実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments, dependent: :destroy
+  # has_many :comments, dependent: :destroy
 
   validates :name,  presence: true
   validates :text,  presence: true
