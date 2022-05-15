79f89dd7c7665654f9dc303a3e82ac45dd09f4b2
SettingTom <tnbt0277@gmail.com>
2022-05-15 16:13:45 +0900

    作品表示順序変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @product = @user.products
+    @product = @user.products.order("created_at DESC")
   end
 end
