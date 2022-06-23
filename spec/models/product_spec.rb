8e2835b52ceb706985257099adde5a2b0e52df22
SettingTom <tnbt0277@gmail.com>
2022-06-23 22:27:27 +0900

    productテストコード語句修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe Product, type: :model do
 
   describe '作品投稿' do
     context '作品投稿できるとき' do
-      it 'name、text、imageが存在すれば登録できる' do
+      it 'name、text、imagesが存在すれば投稿できる' do
         expect(@product).to be_valid
       end
     end
 
     context '作品投稿できないとき' do
-      it 'nameが空では登録できない' do
+      it 'nameが空では投稿できない' do
         @product.name = ''
         @product.valid?
         expect(@product.errors.full_messages).to include("Name can't be blank")
       end
-      it 'textが空では登録できない' do
+      it 'textが空では投稿できない' do
         @product.text = ''
         @product.valid?
         expect(@product.errors.full_messages).to include("Text can't be blank")
       end
-      it 'imageが空では登録できない' do
+      it 'imagesが空では投稿できない' do
         @product.images = nil
         @product.valid?
         expect(@product.errors.full_messages).to include("Images can't be blank")
       end
-      it 'userが紐づいていなければ登録できない' do
+      it 'userが紐づいていなければ投稿できない' do
         @product.user = nil
         @product.valid?
         expect(@product.errors.full_messages).to include('User must exist')
